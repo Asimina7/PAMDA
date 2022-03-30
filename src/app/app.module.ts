707ce440df66d510471abe33b8b97b5772c0053a
PanagiotaAlex <64589521+PanagiotaAlex@users.noreply.github.com>
2022-03-30 21:56:44 +0300

    Add footer, offer, faq, image index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexHeaderComponent } from './index-header/index-header.component';
 import { FeaturesComponent } from './features/features.component';
+import { ImageComponent } from './image/image.component';
+import { FaqComponent } from './faq/faq.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { createPopper } from '@popperjs/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxAccordionModule } from 'igniteui-angular';
+import { IgxSwitchModule } from "igniteui-angular";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexHeaderComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    ImageComponent,
+    FaqComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule, 
+    BrowserAnimationsModule, 
+    IgxAccordionModule, 
+    IgxSwitchModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
