07e12e3f2e5e59152cf093c825c5585ab4f7db84
asimina a <asiminaala@gmail.com>
2022-03-30 20:34:43 +0300

    Added index header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexHeaderComponent } from './index-header/index-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexHeaderComponent
   ],
   imports: [
     BrowserModule,
